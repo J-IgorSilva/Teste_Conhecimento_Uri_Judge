9077fe7942a4ee5edc4e3afced9efea716ed987c
J-IgorSilva <aleatoria8821@gmail.com>
2022-01-28 19:53:22 -0300

    Operation sum of two Values

---
+++ Uri_Judge_1001/Teste/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Teste
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Digite 2 Valores: ");
+            int x, a, b;
+            a = int.Parse(Console.ReadLine());
+            b = int.Parse(Console.ReadLine());
+
+            x = a + b;
+            
+            Console.WriteLine($"A Soma dos Valores: {a} , {b}" + " é Igual a "+ x);
+        }
+    }
+}
