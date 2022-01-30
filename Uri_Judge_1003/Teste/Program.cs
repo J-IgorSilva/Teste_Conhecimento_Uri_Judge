a428392701111101f3706fe23b1209666434dbaa
J-IgorSilva <aleatoria8821@gmail.com>
2022-01-29 21:12:28 -0300

    Simple Product

---
+++ Uri_Judge_1003/Teste/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Teste
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // SOMA DE DOIS VALORES
+            int a, b, x = 0;
+            a = int.Parse(Console.ReadLine());
+            Console.WriteLine("O valor de a: " + a);
+
+            b = int.Parse(Console.ReadLine());
+            Console.WriteLine("O valor de b : " + b);
+
+            x = a + b;
+            Console.WriteLine("Resultado de X : " + x);
+        }
+    }
+}
