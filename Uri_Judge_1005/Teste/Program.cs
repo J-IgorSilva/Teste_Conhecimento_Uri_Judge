2268d0cb81ef5557ce36e8723d72389a5501af11
J-IgorSilva <aleatoria8821@gmail.com>
2022-01-31 23:44:24 -0300

    Avarenge Student

---
+++ Uri_Judge_1005/Teste/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Teste
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double a, b, media;
+            Console.WriteLine("Qual a nota AP1 de João: ");
+            a = double.Parse(Console.ReadLine());
+            Console.WriteLine("Qual a nota AP2 de João: ");
+            b = double.Parse(Console.ReadLine());
+            media = a + b / 2;
+            Console.WriteLine("A Media de João : " + media);
+            Console.ReadLine();
+        }
+    }
+}
