5968dbd0accdb4df1aa4af0d7066344542426967
J-IgorSilva <aleatoria8821@gmail.com>
2022-02-04 23:07:21 -0300

    Fisrt Commit

---
+++ Uri_Judge_1008/Teste/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Teste
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //ler numero de um funcionario, as horas trabalhadas
+            //valor pago por horas trabalhas, e calculo de salario
+            string nm;
+            int n, h;
+            double ch, sal = 0.0;
+            Console.WriteLine("Ola CONTRIBUINTE, Qual o Nome  Do Funcionario: ");
+            nm = Console.ReadLine();
+            Console.WriteLine("Qual Numero De: " + nm);
+            n = int.Parse(Console.ReadLine());
+            Console.WriteLine("Quantas horas " + nm + " Trabalhou: ");
+            h = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Qual Preço Atual da Hora Trabalhada: ");
+            ch = double.Parse(Console.ReadLine());
+
+            sal = h * ch; // hora trabalha x o custo da hora
+
+            Console.WriteLine(nm + " Recebera um valor de: " + sal.ToString("F2"));
+            Console.ReadLine();
+
+        }
+    }
+}
