090e03e4aee885b8902eeae1cdee963211159534
J-IgorSilva <aleatoria8821@gmail.com>
2022-02-08 22:18:46 -0300

    First Commit

---
+++ Uri_Judge_1010/Testes/Produto.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+
+namespace Testes
+{
+    class Produto
+    {
+        public string Nome;
+        public int Quantidade;
+        public double Valor;
+        public double ValorPeca()
+        {
+         return Quantidade * Valor;
+        }
+
+
+    }
+}
---
+++ Uri_Judge_1010/Testes/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Testes
+{
+    class Program : Produto
+    {
+        static void Main(string[] args)
+        {
+
+            Produto P, Vt;
+            Vt = new Produto();
+            P = new Produto();
+            Console.WriteLine("Insira o nome do produto: ");
+            P.Nome= (Console.ReadLine());
+            Console.WriteLine("Qual a quantida desejada: ");
+            P.Quantidade = int.Parse(Console.ReadLine());
+            Console.WriteLine("qual valor de cada peça: ");
+            P.Valor = double.Parse(Console.ReadLine());
+   
+
+            Console.WriteLine("valor total: " + P.ValorPeca());
+
+
+        }
+    }
+}
++
