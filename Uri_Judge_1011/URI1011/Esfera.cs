530365f81f517fb1e89387454edccd15c6ab06d4
J-IgorSilva <aleatoria8821@gmail.com>
2022-02-10 21:16:23 -0300

    Fisrt Commit

---
+++ Uri_Judge_1011/URI1011/Esfera.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace URI1011
+{
+    class Esfera
+    {
+        public double Raio;
+        public double Volume;
+        public double pi = 3.14;
+
+        public double VolumeEsfera()
+        {
+            return (4.0 * pi *Raio*Raio*Raio / 3);
+        }
+
+    }
+}
---
+++ Uri_Judge_1011/URI1011/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace URI1011
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Esfera r;
+            r = new Esfera();
+            Console.WriteLine("Insira o Valor do raio ");
+            r.Raio = double.Parse(Console.ReadLine());
+            Console.WriteLine("O volume exato é: " + r.VolumeEsfera().ToString("F2"));
+        }
+    }
+}
