444362817e12f315a7e4facb78ba36809532afd0
J-IgorSilva <aleatoria8821@gmail.com>
2022-02-11 22:11:50 -0300

    First Commit

---
+++ Uri_Judge_1012/URI 1012/Area.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace URI_1012
+{
+    class Area
+    {
+        public string Triangulo, Circulo, Trapezio, Quadrado, Retangulo;
+        public double A, B, C, RS;
+
+        public double CalculoArea(string a)
+        {
+            switch (CalculoArea(a))
+            {
+                case 1:
+                    a = Triangulo;
+                    Console.WriteLine("Triangulo: " + RS);
+                    RS = A * C / 2;
+                    break;
+                case 2:
+                    a = Circulo;
+                    RS = C * C * 3.14;
+                    Console.WriteLine("Circulo: " + RS);
+                    break;
+                        case 3:
+                    a = Trapezio;
+                    RS = A + B * C / 2;
+                    Console.WriteLine("Trapezio" + RS);
+                    break;
+                case 4:
+                    a = Quadrado;
+                    RS = B * B;
+                    Console.WriteLine("Trapezio: " + RS);
+                    break;
+                case 5:
+                    RS = A * B;
+                    a = Retangulo;
+                    Console.WriteLine("Retangulo: " + RS);
+                    break;
+                default:
+                    Console.WriteLine("Selecione uma area ser Calculada");
+                    break;
+
+            }
+            return CalculoArea(a);
+
+        }
+
+    }
+}
---
+++ Uri_Judge_1012/URI 1012/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace URI_1012
+{
+    class Program
+    {
+        static void Main(string[] args)
+        { 
+            Console.WriteLine("Insira as Areas:");
+            Area figuras, a;
+            figuras = new Area();
+            figuras.A = double.Parse(Console.ReadLine());
+            figuras.B = double.Parse(Console.ReadLine());
+            figuras.C = double.Parse(Console.ReadLine());
+            Console.WriteLine();
+            Console.WriteLine("Escolha Qual Figura Deseja Calcular a Area");
+            Console.WriteLine("Triangulo");
+            Console.WriteLine("Retangulo");
+            Console.WriteLine("Circulo");
+            Console.WriteLine("Trapezio");
+            Console.WriteLine("Quadrado");
+
+
+            string b;
+            b = Console.ReadLine();
+            switch (b = figuras.Triangulo) 
+            { 
+                case 1: 
+                    
+            }
+        }
+    }
+}
