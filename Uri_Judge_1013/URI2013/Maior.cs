1dbd0eccf35c5eb4510048aad8dfcef74e072c8f
J-IgorSilva <aleatoria8821@gmail.com>
2022-02-12 22:37:39 -0300

    First Commit

---
+++ Uri_Judge_1013/URI2013/Maior.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace URI2013
+{
+    class Maior
+    {
+        public int A, B, C;
+        public  int NumeroMaior()
+        {
+            if (A > B)
+                Console.WriteLine("O Mario e´A: " + A);
+            if (B > C)
+            {
+                Console.WriteLine("Maior é B" + B);
+            }
+            else
+            {
+                Console.WriteLine("O maior é C:" + C);
+            }
+            return 0;
+        }
+    }
+}
---
+++ Uri_Judge_1013/URI2013/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace URI2013
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Maior a;
+            a = new Maior();
+            Console.WriteLine("Digite 3");
+            a.A = int.Parse(Console.ReadLine());
+            a.B= int.Parse(Console.ReadLine());
+            a.C= int.Parse(Console.ReadLine());
+            
+            Console.WriteLine("Numeor maior " + a.NumeroMaior());
+            Console.ReadLine();
+        }
+    }
+}
