736a553e31cc9c154e2469b3e7b120a3a216734a
J-IgorSilva <aleatoria8821@gmail.com>
2022-02-13 22:29:18 -0300

    Fisrt Commit

---
+++ Uri_Judge_1014/Consumo/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Consumo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Carro c;
+            c = new Carro();
+            Console.WriteLine("Digite Quantos Km foram Percorridos:___");
+            c.Kilometro = int.Parse(Console.ReadLine());
+            Console.WriteLine("Digite Quantos Litros Foram Usados:___");
+            c.Combustivel = double.Parse(Console.ReadLine());
+            double gasto = 0.0;
+
+            Console.WriteLine("O Gasto total Foi de " + c.DCarro(gasto).ToString("F3)"+ "Km/l"));
+            Console.ReadKey();
+
+        }
+    }
+}
