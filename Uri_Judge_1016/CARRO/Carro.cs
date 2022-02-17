6c54060230c2de14d31846fcdf1d40f91bff1f1e
J-IgorSilva <aleatoria8821@gmail.com>
2022-02-17 19:41:21 -0300

    First commit

---
+++ Uri_Judge_1016/CARRO/Carro.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CARRO
+{
+    class Carro
+    {
+        public int Car1, Car2, km, hora;
+        public int Kilometro(int distancia=0)
+        {
+            distancia = km / hora;
+            return distancia;
+        }
+    
+    }
+   
+}
---
+++ Uri_Judge_1016/CARRO/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CARRO
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Carro c;
+            c = new Carro();
+
+            Console.WriteLine("Distancia do carro 1 :");
+            c.Car1 = int.Parse(Console.ReadLine());
+            Console.WriteLine("Distancia do Carro 2 :");
+            c.Car2 = int.Parse(Console.ReadLine());
+            Console.WriteLine("Quantos minutos de diferenca entre o Primeio e Segundo Carro: ");
+            c.hora = int.Parse(Console.ReadLine());
+            Console.WriteLine("Qual a diferança em kilometros do primeiro para o segundo");
+            c.km = int.Parse(Console.ReadLine());
+            Console.WriteLine(" a diferenca é " + c.Kilometro());
+            Console.ReadLine();
+        }
+    }
+}
