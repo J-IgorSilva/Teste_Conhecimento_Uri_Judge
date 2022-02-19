290bcf570e0ec12ca12e416bae3dd22d4cab1aa4
J-IgorSilva <aleatoria8821@gmail.com>
2022-02-18 21:10:05 -0300

    Fisrt Commit

---
+++ Uri_Judge_1017/Combustível/Gasto.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Combustível
+{
+    class Gasto
+    {
+        public int mLitro, time, speed;
+
+        public int Viagem(double Liters)
+        {
+            Liters = mLitro * speed / time;
+            return Viagem(Liters);
+        }
+    }
+}
---
+++ Uri_Judge_1017/Combustível/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Combustível
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Gasto g;
+            g = new Gasto();
+            Console.WriteLine("Insira os Dados a seguir");
+            Console.WriteLine("Quantos KM por Litro: ");
+
+        }
+    }
+}
